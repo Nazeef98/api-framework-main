38ebab25d16aa0a6ecb78f95adc67c7febe9279d
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-12-06 02:02:44 -0500

    adding the assignment-3 files to the project and making the new changes to it for assignment -4

---
+++ controller/userController.js
@@ -0,0 +1,56 @@
+// userController.js
+/// Nazeef Ahmad Farooqui
+// 200590966
+// 6/12/24
+
+
+const express = require('express');
+const jwt = require('jsonwebtoken'); 
+const User = require('../models/user');
+
+const JWT_SECRET = 'nazeefkey';
+
+
+//logic for register
+const register = (req, res) => {
+    console.log(req.body)
+    User.create(req.body).then(records => {
+        res.json(records)
+    }).catch(error => {
+        console.log(error);
+        res.json({
+            error
+        })
+
+    })
+}
+//logic for login
+const login = (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ email, password }).then(user => {
+        if (user) {
+             //logic to get token and with the logic that expires in  1 hour
+            const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+            res.status(200).json({ message: 'Login successful', token }); // Respond with token
+        } else {
+            res.status(401).json({ message: 'User not found' });
+        }
+    }).catch(error => {
+        console.log(error);
+        res.json({ error });
+    });
+};
+
+//logi for logout
+const logout = (req, res) => {
+
+    res.json({message: 'logout successfull'})
+
+    }
+
+
+module.exports = {
+    register,
+    login,
+    logout
+}
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+// authMiddleware.js
+// Nazeef Ahmad Farooqui
+// 200590966
+// 6/12/24
+
+
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'nazeefkey';
+
+//this code for getting the token after successfull login not implemented for the front end only working in the postman for now
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).json({ message: 'Token is required' });
+
+    //error code that comes up if the token return request is invalid
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
